5f3e6c6dc08c90d75602b9399afd8af21fe4b4f9
Jason <jason.huang11@hotmail.ca>
2017-04-09 01:18:22 -0400

    Major Routing Changes, DB working, Pulling Value
    -When the name is entered in the tourney form, it will save in the mongo
    DB, and show it on the brackets page
    Help from Kyle, Thank you Kyle
    
    -Fixed a routing issue with the refresh, will now redirect to user login
    without having the server to crash.
    
    -NOTE: in the server/routes/brackets.js the code there will yield an
    ERROR. NEED to find another way to update and insert the player into the
    next round (problem arrises DUE to nested schema)
    
    -Added tourney8 schema
    
    -Commented out css to make the bracket page show the results better

--- server/routes/bracket.js
+++ server/routes/bracket.js
@@ -5,13 +5,18 @@ let mongoose = require('mongoose');
 let Tourney4 = require('../models/tourney4');
 //let Tournament4  = Tourney4.Tournament4;
 
+//let Tourney = new Tourney4();
 //check if authenticated
 function requireAuth(req, res, next) {
     if (!req.isAuthenticated()) {
-        return res.redirect('user/login');
+        return res.redirect('/user/login');
     }
     next();
 }
+
+router.get('/', (req, res, next) => {
+    res.render('content/bracket');
+})
 
 /* GET tourney page. */
 router.get('/tourney', requireAuth, (req, res, next) => {
@@ -29,7 +34,7 @@ router.post('/tourney', requireAuth, (req, res, next) => {
     //    "player3": req.body.player3,
     //    "player4": req.body.player4
     // });
-    console.log(req.body.player4);
+    //console.log(req.body.player4);
     let object = {
         'rounds': [{
             'round1': [{
@@ -64,31 +69,84 @@ router.post('/tourney', requireAuth, (req, res, next) => {
         }],
         userID: String
     }
-    console.log(JSON.stringify(object))
+    //console.log(JSON.stringify(object))
     let newTourney = Tourney4(object)
+    //Tourney = newTourney;
 
     Tourney4.create(newTourney, (err, Tourney4) => {
         console.log(JSON.stringify(Tourney4));
         if (err) {
             res.end(err);
         } else {
-            res.redirect('/bracket/bracket');
+            res.redirect('/bracket');
         }
     })
 });
 
 /* GET bracket page. */
-router.get('/bracket', requireAuth, (req, res, next) => {
-    res.render('content/bracket', {
-        title: 'Tournament Bracket',
-        username: req.user ? req.user.username : '',
-        newTourney: newTourney
-    });
+router.get('/:id', requireAuth, (req, res, next) => {
+    /* res.render('content/bracket', {
+         title: 'Tournament Bracket',
+         username: req.user ? req.user.username : '',
+         Tourney: newTourney
+     });*/
+    console.log("before try");
+    try {
+        // get a reference to the id from the url
+        let id = mongoose.Types.ObjectId.createFromHexString(req.params.id);
+
+        // find one game by its id
+        console.log("before find");
+        Tourney4.findById(id, (err, newTourney) => {
+            console.log("Inside if");
+            if (err) {
+                console.log(err);
+                res.end(error);
+            } else {
+                // show the game details view
+                console.log(newTourney);
+                res.render('content/bracket', {
+                    title: 'Tournament Bracket',
+                    username: req.user ? req.user.username : '',
+                    Tourney: newTourney
+                });
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        res.redirect('/errors/404');
+    }
 });
+//});
 
 /* POST bracket page */
-router.post('/bracket', requireAuth, (req, res, next) => {
-    res.redirect('/'); // redirect to homepage
+router.post('/:id', /*requireAuth,*/ (req, res, next) => {
+    try {
+        let id = mongoose.Types.ObjectId.createFromHexString(req.params.id);
+
+        Tourney4.update({ _id:id}, {
+            $push: {
+                "round2": [{ 
+                    "pair3": [{ 
+                        "playerName": req.body.name, 
+                        "Wins": req.body.win, 
+                        "Losses": req.body.loss 
+                    }] 
+                }]
+            }
+        },{upsert:"true"},function(err){
+            if (err){
+               res.json({"text":"ERRROR"});                 
+            } else{            
+                res.json({"text":"HI"});
+            }
+        })
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+
 });
 
 module.exports = router;
